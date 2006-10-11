4928ad1d61c3c884a1382e2feda7d0576741ceee
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2006-10-11 12:38:53 +0000

    Prevent infinite call of redirect-program, if it located in "%SystemRoot%\System32"
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@41 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_setup/Redirect/Source/Redirect.cpp
+++ akelpad_setup/Redirect/Source/Redirect.cpp
@@ -1,12 +1,15 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+int GetExeDirA(HINSTANCE hInstance, char *szExeDir, int nLen);
+int GetExeDirW(HINSTANCE hInstance, wchar_t *wszExeDir, int nLen);
 char* GetCommandLineParamsA();
 wchar_t* GetCommandLineParamsW();
 
+HINSTANCE hInstance;
+BOOL bOldWindows;
 char szCmdLine[32768];
 wchar_t wszCmdLine[32768];
-BOOL bOldWindows;
 
 void _WinMain()
 {
@@ -19,72 +22,104 @@ void _WinMain()
   {
     STARTUPINFO si={0};
     PROCESS_INFORMATION pi={0};
+    char szExeDir[MAX_PATH];
     char szSystemDir[MAX_PATH];
     char *pCmdLineParams;
 
-    si.cb=sizeof(STARTUPINFO);
-    GetStartupInfo(&si);
-
+    hInstance=GetModuleHandle(NULL);
+    GetExeDirA(hInstance, szExeDir, MAX_PATH);
     GetSystemDirectory(szSystemDir, MAX_PATH);
-    wsprintf(szCmdLine, "\"%s\\notepad.exe\" ", szSystemDir);
-    pCmdLineParams=GetCommandLineParamsA();
-    lstrcat(szCmdLine, pCmdLineParams);
 
-    if (CreateProcess(NULL, szCmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    if (lstrcmpi(szExeDir, szSystemDir))
     {
-      CloseHandle(pi.hProcess);
-      CloseHandle(pi.hThread);
+      si.cb=sizeof(STARTUPINFO);
+      GetStartupInfo(&si);
+
+      wsprintf(szCmdLine, "\"%s\\notepad.exe\" ", szSystemDir);
+      pCmdLineParams=GetCommandLineParamsA();
+      lstrcat(szCmdLine, pCmdLineParams);
+
+      if (CreateProcess(NULL, szCmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+      {
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+      }
     }
   }
   else
   {
     STARTUPINFOW siW={0};
     PROCESS_INFORMATION pi={0};
+    wchar_t wszExeDir[MAX_PATH];
     wchar_t wszSystemDir[MAX_PATH];
     wchar_t *wpCmdLineParams;
 
-    siW.cb=sizeof(STARTUPINFOW);
-    GetStartupInfoW(&siW);
-
+    hInstance=GetModuleHandle(NULL);
+    GetExeDirW(hInstance, wszExeDir, MAX_PATH);
     GetSystemDirectoryW(wszSystemDir, MAX_PATH);
-    wsprintfW(wszCmdLine, L"\"%s\\notepad.exe\" ", wszSystemDir);
-    wpCmdLineParams=GetCommandLineParamsW();
-    lstrcatW(wszCmdLine, wpCmdLineParams);
 
-    if (CreateProcessW(NULL, wszCmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &siW, &pi))
+    if (lstrcmpiW(wszExeDir, wszSystemDir))
     {
-      CloseHandle(pi.hProcess);
-      CloseHandle(pi.hThread);
+      siW.cb=sizeof(STARTUPINFOW);
+      GetStartupInfoW(&siW);
+
+      wsprintfW(wszCmdLine, L"\"%s\\notepad.exe\" ", wszSystemDir);
+      wpCmdLineParams=GetCommandLineParamsW();
+      lstrcatW(wszCmdLine, wpCmdLineParams);
+
+      if (CreateProcessW(NULL, wszCmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &siW, &pi))
+      {
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+      }
     }
   }
 
   ExitProcess(0);
 }
 
+int GetExeDirA(HINSTANCE hInstance, char *szExeDir, int nLen)
+{
+  nLen=GetModuleFileName(hInstance, szExeDir, nLen);
+  while (nLen > 0 && szExeDir[nLen] != '\\') --nLen;
+  szExeDir[nLen]='\0';
+
+  return nLen;
+}
+
+int GetExeDirW(HINSTANCE hInstance, wchar_t *wszExeDir, int nLen)
+{
+  nLen=GetModuleFileNameW(hInstance, wszExeDir, nLen);
+  while (nLen > 0 && wszExeDir[nLen] != '\\') --nLen;
+  wszExeDir[nLen]='\0';
+
+  return nLen;
+}
+
 char* GetCommandLineParamsA()
 {
-  char *lpCmdLine=GetCommandLine();
+  char *pCmdLine=GetCommandLine();
 
-  if (*lpCmdLine++ == '\"')
-    while (*lpCmdLine != '\"' && *lpCmdLine != '\0') ++lpCmdLine;
+  if (*pCmdLine++ == '\"')
+    while (*pCmdLine != '\"' && *pCmdLine != '\0') ++pCmdLine;
   else
-    while (*lpCmdLine != ' ' && *lpCmdLine != '\0') ++lpCmdLine;
-  if (*lpCmdLine != '\0')
-    while (*++lpCmdLine == ' ');
+    while (*pCmdLine != ' ' && *pCmdLine != '\0') ++pCmdLine;
+  if (*pCmdLine != '\0')
+    while (*++pCmdLine == ' ');
 
-  return lpCmdLine;
+  return pCmdLine;
 }
 
 wchar_t* GetCommandLineParamsW()
 {
-  wchar_t *lpwCmdLine=GetCommandLineW();
+  wchar_t *wpCmdLine=GetCommandLineW();
 
-  if (*lpwCmdLine++ == '\"')
-    while (*lpwCmdLine != '\"' && *lpwCmdLine != '\0') ++lpwCmdLine;
+  if (*wpCmdLine++ == '\"')
+    while (*wpCmdLine != '\"' && *wpCmdLine != '\0') ++wpCmdLine;
   else
-    while (*lpwCmdLine != ' ' && *lpwCmdLine != '\0') ++lpwCmdLine;
-  if (*lpwCmdLine != '\0')
-    while (*++lpwCmdLine == ' ');
+    while (*wpCmdLine != ' ' && *wpCmdLine != '\0') ++wpCmdLine;
+  if (*wpCmdLine != '\0')
+    while (*++wpCmdLine == ' ');
 
-  return lpwCmdLine;
+  return wpCmdLine;
 }
