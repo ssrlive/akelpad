8eb80231f6ad7175ea02a10f284bf37cc727f891
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2008-06-15 20:41:07 +0000

    Added: unicode files without BOM now marked as "*BOM" in status bar. Added: EM_GETSCROLLPOS, EM_SETSCROLLPOS messages.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@154 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/Edit.h
+++ akelpad_4/Edit.h
@@ -33,6 +33,8 @@
 #define STR_UNICODE_UTF8W     L"65001 (UTF-8)"
 #define STR_UNICODE_UTF7A      "65000 (UTF-7)"
 #define STR_UNICODE_UTF7W     L"65000 (UTF-7)"
+#define STR_NOBOMA             "  *BOM"
+#define STR_NOBOMW            L"  *BOM"
 
 #ifndef BIF_NEWDIALOGSTYLE
   #define BIF_NEWDIALOGSTYLE 0x0040
