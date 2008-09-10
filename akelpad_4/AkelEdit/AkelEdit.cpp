98796c3d7824425ebfdc052b3ca3ff5de94ae196
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2008-09-10 11:36:46 +0000

    4.0.5 -> 4.0.6

--- akelpad_4/AkelEdit/AkelEdit.cpp
+++ akelpad_4/AkelEdit/AkelEdit.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************
- *                      AkelEdit text control v1.5                                 *
+ *                      AkelEdit text control v1.6                                 *
  *                                                                                 *
  * Copyright 2007-2008 by Shengalts Aleksander aka Instructor (Shengalts@mail.ru)  *
  *                                                                                 *
