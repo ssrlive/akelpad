6ff0a4d5bd347633bfa9d5d9f90a95e4f2e7fd9e
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2008-10-26 05:44:14 +0000

    Changed: improved work with Input Method Editors (IME).
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@457 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelEdit/Build.cmd
+++ akelpad_4/AkelEdit/Build.cmd
@@ -8,7 +8,7 @@ Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
 Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
 
 rc /r /Fo"AkelEdit.res" "Resources\AkelEdit.rc"
-cl /O1 AkelEdit.cpp AkelEdit.res /LD /link kernel32.lib user32.lib gdi32.lib ole32.lib uuid.lib libc.lib /OPT:NOWIN98 /NODEFAULTLIB /ENTRY:DllMain /OUT:AkelEdit.dll
+cl /O1 AkelEdit.cpp AkelEdit.res /LD /link kernel32.lib user32.lib gdi32.lib ole32.lib uuid.lib imm32.lib libc.lib /OPT:NOWIN98 /NODEFAULTLIB /ENTRY:DllMain /OUT:AkelEdit.dll
 
 if exist AkelEdit.res del AkelEdit.res
 if exist AkelEdit.obj del AkelEdit.obj
