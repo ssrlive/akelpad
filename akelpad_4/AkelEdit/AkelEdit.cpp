2dfb3d51bedd5a6652f785b5418973c85840fb3a
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-03-10 09:35:59 +0000

    Improved: EM_POSFROMCHAR as RichEdit 2.0.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@769 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelEdit/AkelEdit.cpp
+++ akelpad_4/AkelEdit/AkelEdit.cpp
@@ -1492,7 +1492,7 @@ LRESULT CALLBACK AE_EditProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
       AECHARINDEX ci={0};
 
-      if (lParam || !IsBadCodePtr((FARPROC)wParam))
+      if (lParam || !IsBadWritePtr((FARPROC)wParam, sizeof(POINT)))
       {
         //RichEdit 3.0 syntax
         POINT *pt=(POINT *)wParam;
