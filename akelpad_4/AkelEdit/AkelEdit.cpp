3505d3e43ed75171976cdbaee849b3d5256ffd29
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-03-18 12:28:18 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@789 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelEdit/AkelEdit.cpp
+++ akelpad_4/AkelEdit/AkelEdit.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************
- *                      AkelEdit text control v1.1.9                               *
+ *                      AkelEdit text control v1.2.0                               *
  *                                                                                 *
  * Copyright 2007-2009 by Shengalts Aleksander aka Instructor (Shengalts@mail.ru)  *
  *                                                                                 *
