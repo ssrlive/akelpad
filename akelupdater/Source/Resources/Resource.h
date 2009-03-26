916962ddd65ae82ab702fbdfcc8dea1013a2dbf3
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-03-26 13:15:04 +0000

    Initial AkelUpdater release.

---
+++ akelupdater/Source/Resources/Resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by AkelUpdater.rc
+//
+#define IDD_SETUP                       1001
+#define IDI_ICON                        1051
+#define IDC_LIST_EXE                    1101
+#define IDC_LIST_DLL                    1102
+#define IDC_LIST_INFO                   1103
+#define IDC_SELECTED                    1104
+#define IDC_MIRROR                      1105
+#define IDC_LANGUAGE                    1106
+#define IDC_MIRROR_LABEL                1107
+#define IDC_LANGUAGE_LABEL              1108
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        11001
+#define _APS_NEXT_COMMAND_VALUE         12001
+#define _APS_NEXT_CONTROL_VALUE         13001
+#define _APS_NEXT_SYMED_VALUE           14001
+#endif
+#endif
