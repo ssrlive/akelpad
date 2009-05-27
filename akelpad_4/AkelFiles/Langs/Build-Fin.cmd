0587bbddd688262f6d4f0a632e9f36fe91036935
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-05-27 09:41:15 +0000

    Added: Finnish language module (Jani Kinnunen).

---
+++ akelpad_4/AkelFiles/Langs/Build-Fin.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+Set MSSDK=D:\Program Files\Microsoft Platform SDK
+Set VCDIR=D:\Program Files\Microsoft Visual C++ Toolkit 2003
+Set LANG=Finnish
+
+::###################################::
+Set PATH=%VCDIR%\bin;%MSSDK%\bin;%PATH%
+Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
+Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
+
+rc /R /I "..\..\AkelEdit\Resources" /Fo"%LANG%.res" "Resources\%LANG%.rc"
+cl /O1 Module.cpp /LD /link "%LANG%.res" /OPT:NOWIN98 /MACHINE:I386 /NODEFAULTLIB /ENTRY:DllMain /OUT:"%LANG%.dll"
+
+if exist Module.obj del Module.obj
+if exist "%LANG%.res" del "%LANG%.res"
+@PAUSE
