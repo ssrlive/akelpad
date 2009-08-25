d8187f595e689db97a9fb007209831c2c84ba1cc
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-08-25 14:55:24 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@1066 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/Edit.cpp
+++ akelpad_4/Edit.cpp
@@ -11734,7 +11734,7 @@ BOOL ColumnPaste(HWND hWnd)
 BOOL PasteAfter(HWND hWnd, BOOL bAnsi)
 {
   CHARRANGE cr;
-  
+
   SendMessage(hWnd, EM_EXGETSEL, 0, (LPARAM)&cr);
   if (DoEditPaste(hWnd, bAnsi))
   {
@@ -20916,15 +20916,23 @@ void UpdateTabs(HWND hWnd)
 int GetTabItemFromParam(HWND hWnd, LPARAM lParam)
 {
   TCITEMA tcItemA;
-  int i=-1;
+  int nCurSel;
+  int i;
 
   tcItemA.mask=TCIF_PARAM;
+  nCurSel=SendMessage(hWnd, TCM_GETCURSEL, 0, 0);
+  SendMessage(hWnd, TCM_GETITEMA, nCurSel, (LPARAM)&tcItemA);
+  if (tcItemA.lParam == lParam) return nCurSel;
 
   for (i=0; SendMessage(hWnd, TCM_GETITEMA, i, (LPARAM)&tcItemA); ++i)
   {
-    if (tcItemA.lParam == lParam) break;
+    if (i != nCurSel)
+    {
+      if (tcItemA.lParam == lParam)
+        return i;
+    }
   }
-  return i;
+  return -1;
 }
 
 int GetTabItemFromPoint(HWND hWnd, POINT *pt)
