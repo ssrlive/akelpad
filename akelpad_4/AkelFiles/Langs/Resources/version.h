d0b34eb515f03a5143fd2fc4c34e514cd947052f
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-11-22 07:54:39 +0000

    Changed: one Version.rc for EXE and DLL.

--- akelpad_4/AkelFiles/Langs/Resources/version.h
+++ akelpad_4/AkelFiles/Langs/Resources/version.h
@@ -2,11 +2,21 @@
 
 #define AKELPAD_ID 4,3,6,0
 
-#ifdef EXE_VERSION
-  #ifndef EXE_VERSION_INCLUDED
+#if defined(EXE_VERSION)
+  #if !defined(EXE_VERSION_INCLUDED) && !defined(DLL_VERSION_INCLUDED)
+    #define RC_VERSIONDESCRIPTION "AkelPad text editor"
+    #define RC_VERSIONCOPYRIGHT "Copyright © AkelSoft 2003-2009"
+    #define RC_VERSIONNAME "AkelPad.exe"
     #undef _MAC
     #include "Version.rc"
     #define _MAC
     #define EXE_VERSION_INCLUDED
+  #endif
+#elif defined(DLL_VERSION)
+  #if !defined(DLL_VERSION_INCLUDED) && !defined(EXE_VERSION_INCLUDED)
+    #undef _MAC
+    #include "Version.rc"
+    #define _MAC
+    #define DLL_VERSION_INCLUDED
   #endif
 #endif
