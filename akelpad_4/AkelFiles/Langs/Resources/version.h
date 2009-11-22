f80951b0704ed68c02deff0615aa72a7802683d9
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2009-11-22 14:09:20 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@1278 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelFiles/Langs/Resources/version.h
+++ akelpad_4/AkelFiles/Langs/Resources/version.h
@@ -1,7 +1,7 @@
 #define AKELPAD_ID 4,3,6,0
 
-#if defined(EXE_VERSION)
-  #if !defined(EXE_VERSION_INCLUDED) && !defined(DLL_VERSION_INCLUDED)
+#if !defined(EXE_VERSION_INCLUDED) && !defined(DLL_VERSION_INCLUDED)
+  #if defined(EXE_VERSION)
     #define RC_VERSIONDESCRIPTION "AkelPad text editor"
     #define RC_VERSIONCOPYRIGHT "Copyright © AkelSoft 2003-2009"
     #define RC_VERSIONNAME "AkelPad.exe"
@@ -9,9 +9,7 @@
     #include "Version.rc"
     #define _MAC
     #define EXE_VERSION_INCLUDED
-  #endif
-#elif defined(DLL_VERSION)
-  #if !defined(DLL_VERSION_INCLUDED) && !defined(EXE_VERSION_INCLUDED)
+  #elif defined(DLL_VERSION)
     #undef _MAC
     #include "Version.rc"
     #define _MAC
