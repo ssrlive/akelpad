922868fa559a95afce0536dba143974cd8367089
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-04-24 06:29:10 +0000

    Removed libc.lib dependency.

--- akelpad_4/Dinamic-Build-Eng.cmd
+++ akelpad_4/Dinamic-Build-Eng.cmd
@@ -9,7 +9,7 @@ Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
 Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
 
 rc /R /DEXE_VERSION /Fo"AkelPad.res" "AkelFiles\Langs\Resources\%LANG%.rc"
-cl /O1 AkelPad.cpp Edit.cpp AkelPad.res /link kernel32.lib user32.lib gdi32.lib advapi32.lib shell32.lib comctl32.lib comdlg32.lib version.lib libc.lib /SUBSYSTEM:WINDOWS /OPT:NOWIN98 /MACHINE:I386 /NODEFAULTLIB /ENTRY:_WinMain /OUT:AkelPad.exe
+cl /O1 AkelPad.cpp Edit.cpp AkelPad.res /link kernel32.lib user32.lib gdi32.lib advapi32.lib shell32.lib comctl32.lib comdlg32.lib version.lib /SUBSYSTEM:WINDOWS /OPT:NOWIN98 /MACHINE:I386 /NODEFAULTLIB /ENTRY:_WinMain /OUT:AkelPad.exe
 
 if exist AkelPad.obj del AkelPad.obj
 if exist Edit.obj del Edit.obj
