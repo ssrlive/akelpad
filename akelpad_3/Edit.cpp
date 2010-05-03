b983c140968f5a81dda3a99d066fd243b46a8d37
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-05-03 13:09:43 +0000

    Added: fast saving or reopening of the new, not saved document in different encoding now leads to a current encoding change.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@1575 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_3/Edit.cpp
+++ akelpad_3/Edit.cpp
@@ -4662,6 +4662,12 @@ int SaveDocumentA(HWND hWnd, char *szFile, int nCodePage, BOOL bBOM, DWORD dwFla
   int nCodePageCmp;
   int nLine=0;
 
+  if (!szFile[0])
+  {
+    SetCodePageStatusA(nCodePage, bBOM, FALSE);
+    return nResult;
+  }
+
   //Notification message
   if (GetWindowLongA(hWnd, GWL_ID) == ID_EDIT)
   {
@@ -4868,6 +4874,12 @@ int SaveDocumentW(HWND hWnd, wchar_t *wszFile, int nCodePage, BOOL bBOM, DWORD d
   int nFileCmp;
   int nCodePageCmp;
   int nLine=0;
+
+  if (!wszFile[0])
+  {
+    SetCodePageStatusW(nCodePage, bBOM, FALSE);
+    return nResult;
+  }
 
   //Notification message
   if (GetWindowLongW(hWnd, GWL_ID) == ID_EDIT)
