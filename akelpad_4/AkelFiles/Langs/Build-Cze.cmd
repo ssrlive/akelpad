1c90b5f219f2902a77db741159c682c32fb1aa2a
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-09-18 06:19:33 +0000

    Added: Czech language module (Frantisek Bartos).
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@1872 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

---
+++ akelpad_4/AkelFiles/Langs/Build-Cze.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+Set MSSDK=D:\Program Files\Microsoft Platform SDK
+Set VCDIR=D:\Program Files\Microsoft Visual C++ Toolkit 2003
+Set LANGNAME=Czech
+Set LANGID=0x405
+
+::###################################::
+Set PATH=%VCDIR%\bin;%MSSDK%\bin;%PATH%
+Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
+Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
+
+rc /R /DDLL_VERSION /DRC_VERSIONLANGID=%LANGID% /I "..\..\AkelEdit\Resources" /Fo"%LANGNAME%.res" "Resources\%LANGNAME%.rc"
+cl /O1 Module.cpp /LD /link "%LANGNAME%.res" /OPT:NOWIN98 /MACHINE:I386 /NODEFAULTLIB /ENTRY:DllMain /OUT:"%LANGNAME%.dll"
+
+if exist Module.obj del Module.obj
+if exist "%LANGNAME%.res" del "%LANGNAME%.res"
+@PAUSE
