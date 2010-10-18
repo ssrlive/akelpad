cf9ab4b3d98322e45669b7a9403f87b83e024b4d
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-10-18 18:21:07 +0000

    Added: plugin copies update.

--- akelupdater/Source/Build.cmd
+++ akelupdater/Source/Build.cmd
@@ -8,7 +8,7 @@ Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
 Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
 
 rc /r /Fo"AkelUpdater.res" "Resources\AkelUpdater.rc"
-cl /O1 AkelUpdater.cpp AkelUpdater.res  /LD /link kernel32.lib user32.lib libc.lib /OPT:NOWIN98 /NODEFAULTLIB /ENTRY:DllMain
+cl /O1 AkelUpdater.c AkelUpdater.res  /LD /link kernel32.lib user32.lib /OPT:NOWIN98 /NODEFAULTLIB /ENTRY:DllMain
 
 if exist AkelUpdater.res del AkelUpdater.res
 if exist AkelUpdater.obj del AkelUpdater.obj
