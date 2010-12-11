b3b4349d2109f759499ebb13e7e4d08167d61f2d
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-12-11 21:24:37 +0000

    Added: support for x64 compilation.

--- akelpad_4/Dinamic-Build-Eng.cmd
+++ akelpad_4/Dinamic-Build-Eng.cmd
@@ -3,13 +3,14 @@ Set MSSDK=D:\Program Files\Microsoft Platform SDK
 Set VCDIR=D:\Program Files\Microsoft Visual C++ Toolkit 2003
 Set LANGNAME=English
 Set LANGID=0x409
+Set BIT=32
 
 ::###################################::
 Set PATH=%VCDIR%\bin;%MSSDK%\bin;%PATH%
 Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
 Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
 
-rc /R /DEXE_VERSION /DRC_VERSIONLANGID=%LANGID% /Fo"AkelPad.res" "AkelFiles\Langs\Resources\%LANGNAME%.rc"
+rc /R /DEXE_VERSION /DRC_VERSIONLANGID=%LANGID% /DRC_VERSIONMANIFEST=%BIT% /Fo"AkelPad.res" "AkelFiles\Langs\Resources\%LANGNAME%.rc"
 cl /O1 /DRC_VERSIONLANGID=%LANGID% AkelPad.c Edit.c AkelPad.res /link kernel32.lib user32.lib gdi32.lib advapi32.lib shell32.lib comctl32.lib comdlg32.lib version.lib /SUBSYSTEM:WINDOWS /OPT:NOWIN98 /MACHINE:I386 /NODEFAULTLIB /ENTRY:_WinMain /OUT:AkelPad.exe
 
 if exist AkelPad.obj del AkelPad.obj
