3bc928ff1f8b5b5f81d3cfa3ac9ceb08ced59ff7
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-12-11 21:24:37 +0000

    Added: support for x64 compilation.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@2036 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/Build-Fra.cmd
+++ akelpad_4/Build-Fra.cmd
@@ -3,13 +3,14 @@ Set MSSDK=D:\Program Files\Microsoft Platform SDK
 Set VCDIR=D:\Program Files\Microsoft Visual C++ Toolkit 2003
 Set LANGNAME=French
 Set LANGID=0x40C
+Set BIT=32
 
 ::###################################::
 Set PATH=%VCDIR%\bin;%MSSDK%\bin;%PATH%
 Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
 Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
 
-rc /R /DAKELEDIT_STATICBUILD /DEXE_VERSION /DRC_VERSIONLANGID=%LANGID% /I "AkelEdit\Resources" /Fo"AkelPad.res" "AkelFiles\Langs\Resources\%LANGNAME%.rc"
+rc /R /DAKELEDIT_STATICBUILD /DEXE_VERSION /DRC_VERSIONLANGID=%LANGID% /DRC_VERSIONMANIFEST=%BIT% /I "AkelEdit\Resources" /Fo"AkelPad.res" "AkelFiles\Langs\Resources\%LANGNAME%.rc"
 cl /O1 /DAKELEDIT_STATICBUILD /DRC_VERSIONLANGID=%LANGID% AkelPad.c Edit.c AkelEdit\AkelEdit.c AkelPad.res /link kernel32.lib user32.lib gdi32.lib advapi32.lib shell32.lib comctl32.lib comdlg32.lib ole32.lib uuid.lib imm32.lib version.lib /SUBSYSTEM:WINDOWS /OPT:NOWIN98 /MACHINE:I386 /NODEFAULTLIB /ENTRY:_WinMain /OUT:AkelPad.exe
 
 if exist AkelPad.obj del AkelPad.obj
