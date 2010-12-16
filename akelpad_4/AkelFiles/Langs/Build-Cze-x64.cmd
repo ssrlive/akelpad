e2f6caeb2715a3224e4f8bc960a04469533c5a70
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-12-16 11:49:09 +0000

    Added: support for x64 compilation.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@2051 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

---
+++ akelpad_4/AkelFiles/Langs/Build-Cze-x64.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+Set MSSDK=D:\Program Files\Microsoft Platform SDK
+Set LANGNAME=Czech
+Set LANGID=0x405
+Set BIT=64
+
+::###################################::
+Set PATH=%MSSDK%\bin\win64\x86\AMD64;%MSSDK%\bin;%PATH%
+Set INCLUDE=%MSSDK%\include;%MSSDK%\include\crt;%INCLUDE%
+Set LIB=%MSSDK%\lib\AMD64;%LIB%
+
+rc /R /DDLL_VERSION /DRC_VERSIONLANGID=%LANGID% /DRC_VERSIONBIT=%BIT% /I "..\..\AkelEdit\Resources" /Fo"%LANGNAME%.res" "Resources\%LANGNAME%.rc"
+cl /O1 /GS- Module.c /LD /link "%LANGNAME%.res" /OPT:NOWIN98 /MACHINE:AMD64 /NODEFAULTLIB /ENTRY:DllMain /OUT:"%LANGNAME%.dll"
+
+if exist Module.obj del Module.obj
+if exist "%LANGNAME%.res" del "%LANGNAME%.res"
+@PAUSE
