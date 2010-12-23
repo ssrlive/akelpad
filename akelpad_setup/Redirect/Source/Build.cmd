3fa9439182c44a361258b0678732723d721586ae
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-12-23 17:14:05 +0000

    Added: x64 support.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@2065 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_setup/Redirect/Source/Build.cmd
+++ akelpad_setup/Redirect/Source/Build.cmd
@@ -1,15 +1,15 @@
 @ECHO OFF
-Set MSSDK=D:\Program Files\Microsoft Platform SDK
-Set VCDIR=D:\Program Files\Microsoft Visual C++ Toolkit 2003
-
+Set MSSDK=C:\Program Files\Microsoft Platform SDK
+Set VCDIR=C:\Program Files\Microsoft Visual C++ Toolkit 2003
+Set BIT=32
 
 ::###################################::
 Set PATH=%VCDIR%\bin;%MSSDK%\bin;%PATH%
-Set INCLUDE=%MSSDK%\Include;%VCDIR%\include;%INCLUDE%
-Set LIB=%MSSDK%\lib;%VCDIR%\lib;%LIB%
+Set INCLUDE=%VCDIR%\include;%MSSDK%\include;%INCLUDE%
+Set LIB=%VCDIR%\lib;%MSSDK%\lib;%LIB%
 
-rc /r /Fo"Redirect.res" "Resources\Redirect.rc"
-cl /O1 Redirect.cpp /link kernel32.lib user32.lib advapi32.lib comdlg32.lib Redirect.res /SUBSYSTEM:WINDOWS /MACHINE:I386 /OPT:NOWIN98 /NODEFAULTLIB /ENTRY:_WinMain /OUT:"notepad.exe"
+rc /R /DRC_VERSIONBIT=%BIT% /Fo"Redirect.res" "Resources\Redirect.rc"
+cl /O1 Redirect.c Redirect.res /link kernel32.lib user32.lib advapi32.lib comdlg32.lib /SUBSYSTEM:WINDOWS /MACHINE:I386 /OPT:NOWIN98 /NODEFAULTLIB /ENTRY:_WinMain /OUT:"notepad.exe"
 
 if exist Redirect.obj del Redirect.obj
 if exist Redirect.res del Redirect.res
