9e9e1cbc071dc1667dbb2d34e4248b6d83a90286
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2010-12-25 20:11:59 +0000

    Better support for different compilers.

--- akelpad_4/AkelFiles/Plugs/AkelDLL/Build-x64.cmd
+++ akelpad_4/AkelFiles/Plugs/AkelDLL/Build-x64.cmd
@@ -18,10 +18,8 @@ if "%BIT%" == "32" (
 )
 
 ::### Compile ###::
-if not "%VCDIR%" == "%VCDIR:VC98=%" (
-  set CLFLAGS=
-) else if "%VCDIR%" == "%VCDIR:2003=%" (
-  set CLFLAGS=%CLFLAGS% /GS-
+if "%VCDIR%" == "%VCDIR:2003=%" (
+  if "%VCDIR%" == "%VCDIR:VC98=%" set CLFLAGS=%CLFLAGS% /GS-
 )
 cl /O1 %CLFLAGS% AkelDLL.c /LD /link kernel32.lib user32.lib /OPT:NOWIN98 /MACHINE:%MACHINE% /NODEFAULTLIB /ENTRY:DllMain
 
