eaecf6a52b56d1bb1b83cf05ab11d77d9d7886e5
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2011-04-30 11:13:47 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@2271 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelEdit/StrFunc.h
+++ akelpad_4/AkelEdit/StrFunc.h
@@ -2394,9 +2394,6 @@ INT_PTR hex2decA(const char *pStrHex)
   INT_PTR a;
   INT_PTR b=0;
 
-  while (*pStrHex == '0')
-    ++pStrHex;
-
   for (;;)
   {
     a=*pStrHex++;
@@ -2431,9 +2428,6 @@ INT_PTR hex2decW(const wchar_t *wpStrHex)
 {
   INT_PTR a;
   INT_PTR b=0;
-
-  while (*wpStrHex == '0')
-    ++wpStrHex;
 
   for (;;)
   {
