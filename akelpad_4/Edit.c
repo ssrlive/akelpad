cca849360ece1a0e3bef2e7986b3f562eefad3a3
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2011-12-08 05:03:44 +0000

    Added: bResidentAkelAdmin to unload AkelAdmin directly after call.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@2597 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/Edit.c
+++ akelpad_4/Edit.c
@@ -176,6 +176,7 @@ extern wchar_t wszAkelAdminExe[MAX_PATH];
 extern wchar_t wszAkelAdminPipe[32];
 extern BOOL bPipeInitAkelAdmin;
 extern BOOL bSetSecurity;
+extern BOOL bResidentAkelAdmin;
 extern HICON hIconShieldAkelAdmin;
 
 //MessageBox dialog
@@ -4325,7 +4326,8 @@ int OpenDocument(HWND hWnd, const wchar_t *wpFile, DWORD dwFlags, int nCodePage,
   if (bSetSecurity)
   {
     if (!AkelAdminSend(AAA_SECURITYRESTORE, L"") ||
-        !AkelAdminSend(AAA_SECURITYFREE, L""))
+        !AkelAdminSend(AAA_SECURITYFREE, L"") ||
+        !bResidentAkelAdmin)
     {
       //Reset AkelAdmin
       AkelAdminExit();
@@ -4989,7 +4991,8 @@ int SaveDocument(HWND hWnd, const wchar_t *wpFile, int nCodePage, BOOL bBOM, DWO
   if (bSetSecurity)
   {
     if (!AkelAdminSend(AAA_SECURITYRESTORE, L"") ||
-        !AkelAdminSend(AAA_SECURITYFREE, L""))
+        !AkelAdminSend(AAA_SECURITYFREE, L"") ||
+        !bResidentAkelAdmin)
     {
       //Reset AkelAdmin
       AkelAdminExit();
