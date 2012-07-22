8fdf5969528a198806cfee06bae4d756787b7c90
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2012-07-22 17:04:52 +0000

    Updated.

--- akelpad_4/AkelFiles/Plugs/AkelDLL/AkelDLL.h
+++ akelpad_4/AkelFiles/Plugs/AkelDLL/AkelDLL.h
@@ -8,7 +8,7 @@
   #define MAKE_IDENTIFIER(a, b, c, d)  ((DWORD)MAKELONG(MAKEWORD(a, b), MAKEWORD(c, d)))
 #endif
 
-#define AKELDLL MAKE_IDENTIFIER(1, 8, 0, 0)
+#define AKELDLL MAKE_IDENTIFIER(1, 8, 0, 1)
 
 
 //// Defines
