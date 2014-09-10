201518fcff9748d9f91d5db58c8f612606f86b01
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2014-09-10 05:55:40 +0000

    Added: send command line on exit.

--- akelpad_4/AkelFiles/Plugs/Exit/Source/Resources/Resource.h
+++ akelpad_4/AkelFiles/Plugs/Exit/Source/Resources/Resource.h
@@ -2,22 +2,24 @@
 // Microsoft Developer Studio generated include file.
 // Used by Exit.rc
 //
-#define IDI_ICON_PLUGIN                 101
-#define IDD_SETUP                       1001
-#define IDC_PROMPT_EXIT                 1101
-#define IDC_PROMPT_EXIT_MSG             1102
-#define IDC_ESCAPE                      1103
-#define IDC_ESC_EXIT                    1104
-#define IDC_ESC_MINIMIZE                1105
-#define IDC_ESC_IGNORE                  1106
-#define IDC_ESC_CLOSE_TAB               1107
-#define IDC_ESC_CLOSE_TAB__ONE_TAB_EXIT 1108
-#define IDC_ESC_CLOSE_TAB__NO_TAB_EXIT  1109
-#define IDC_ESC_CLOSE_TAB__NO_TAB_DONT_EXIT 1110
+#define IDI_ICON_PLUGIN               101
+#define IDD_SETUP                     1001
+#define IDC_PROMPTEXIT_ENABLE         1101
+#define IDC_PROMPTEXIT                1102
+#define IDC_CMDLINE_ENABLE            1103
+#define IDC_CMDLINE                   1104
+#define IDC_ESCAPE_GROUP              1105
+#define IDC_ESCEXIT                   1106
+#define IDC_ESCMINIMIZE               1107
+#define IDC_ESCIGNORE                 1108
+#define IDC_ESCCLOSETAB               1109
+#define IDC_ESCCLOSETAB_ONETABEXIT    1110
+#define IDC_ESCCLOSETAB_NOTABEXIT     1111
+#define IDC_ESCCLOSETAB_NOTABDONTEXIT 1112
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        11001
