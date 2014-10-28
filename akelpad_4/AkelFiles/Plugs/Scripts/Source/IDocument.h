b26da4b742cf88d588be6cb0a33a2404ced4a24b
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2014-10-28 19:28:14 +0000

    Fixed: possible dangling pointer execution after ThreadUnhook.

--- akelpad_4/AkelFiles/Plugs/Scripts/Source/IDocument.h
+++ akelpad_4/AkelFiles/Plugs/Scripts/Source/IDocument.h
@@ -398,6 +398,7 @@ void StackFillMessages(MSGINTSTACK *hStack, SAFEARRAY *psa);
 int RetriveCallbackProc(CALLBACKBUSYNESS *cb);
 CALLBACKITEM* StackInsertCallback(CALLBACKSTACK *hStack, IDispatch *objCallback);
 int StackGetCallbackCount(CALLBACKSTACK *hStack, int nCallbackType);
+BOOL StackIsCallback(CALLBACKSTACK *hStack, void *lpScriptThread);
 CALLBACKITEM* StackGetCallbackByHandle(CALLBACKSTACK *hStack, HANDLE hHandle, void *lpScriptThread);
 CALLBACKITEM* StackGetCallbackByProc(CALLBACKSTACK *hStack, INT_PTR lpProc);
 CALLBACKITEM* StackGetCallbackByObject(CALLBACKSTACK *hStack, IDispatch *objFunction);
