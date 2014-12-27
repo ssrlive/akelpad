3e972d7b6e6cb075f5ca96bda857f6e7091b3b71
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2014-12-27 16:30:31 +0000

    Added: AkelPad.Global.

---
+++ akelpad_4/AkelFiles/Plugs/Scripts/Source/IGlobal.h
@@ -0,0 +1,52 @@
+#ifndef _IGLOBAL_H_
+#define _IGLOBAL_H_
+
+// IGlobal's VTable
+#undef INTERFACE
+#define INTERFACE IGlobal
+DECLARE_INTERFACE_ (INTERFACE, IDispatch)
+{
+  // IUnknown
+  STDMETHOD_(HRESULT, Global_QueryInterface)(THIS_ REFIID, void **) PURE;
+  STDMETHOD_(ULONG, Global_AddRef)(THIS) PURE;
+  STDMETHOD_(ULONG, Global_Release)(THIS) PURE;
+
+  // IDispatch
+  STDMETHOD_(HRESULT, Global_GetTypeInfoCount)(THIS_ UINT *) PURE;
+  STDMETHOD_(HRESULT, Global_GetTypeInfo)(THIS_ UINT, LCID, ITypeInfo **) PURE;
+  STDMETHOD_(HRESULT, Global_GetIDsOfNames)(THIS_ REFIID, LPOLESTR *, UINT, LCID, DISPID *) PURE;
+  STDMETHOD_(HRESULT, Global_Invoke)(THIS_ DISPID, REFIID, LCID, WORD, DISPPARAMS *, VARIANT *, EXCEPINFO *, UINT *) PURE;
+
+  STDMETHOD_(HRESULT, Global_PtrAdd)(THIS_ VARIANT, VARIANT, VARIANT *) PURE;
+  STDMETHOD_(HRESULT, Global_vbPtrAdd)(THIS_ VARIANT, VARIANT, VARIANT *) PURE;
+  STDMETHOD_(HRESULT, Global_PtrCmp)(THIS_ VARIANT, VARIANT, int *) PURE;
+  STDMETHOD_(HRESULT, Global_vbPtrCmp)(THIS_ VARIANT, VARIANT, int *) PURE;
+};
+
+//Defines
+typedef struct {
+  IGlobalVtbl *lpVtbl;
+  DWORD dwCount;
+  void *lpScriptThread;
+} IRealGlobal;
+
+//Global variables
+extern ITypeInfo *g_GlobalTypeInfo;
+extern const IGlobalVtbl MyIGlobalVtbl;
+
+//Functions prototypes
+HRESULT STDMETHODCALLTYPE Global_QueryInterface(IGlobal *this, REFIID vTableGuid, void **ppv);
+ULONG STDMETHODCALLTYPE Global_AddRef(IGlobal *this);
+ULONG STDMETHODCALLTYPE Global_Release(IGlobal *this);
+
+HRESULT STDMETHODCALLTYPE Global_GetTypeInfoCount(IGlobal *this, UINT *pCount);
+HRESULT STDMETHODCALLTYPE Global_GetTypeInfo(IGlobal *this, UINT itinfo, LCID lcid, ITypeInfo **pTypeInfo);
+HRESULT STDMETHODCALLTYPE Global_GetIDsOfNames(IGlobal *this, REFIID riid, LPOLESTR *rgszNames, UINT cNames, LCID lcid, DISPID *rgdispid);
+HRESULT STDMETHODCALLTYPE Global_Invoke(IGlobal *this, DISPID dispid, REFIID riid, LCID lcid, WORD wFlags, DISPPARAMS *params, VARIANT *result, EXCEPINFO *pexcepinfo, UINT *puArgErr);
+
+HRESULT STDMETHODCALLTYPE Global_PtrAdd(IGlobal *this, VARIANT vtPointer1, VARIANT vtPointer2, VARIANT *vtPointerResult);
+HRESULT STDMETHODCALLTYPE Global_vbPtrAdd(IGlobal *this, VARIANT vtPointer1, VARIANT vtPointer2, VARIANT *vtPointerResult);
+HRESULT STDMETHODCALLTYPE Global_PtrCmp(IGlobal *this, VARIANT vtPointer1, VARIANT vtPointer2, int *nCmpResult);
+HRESULT STDMETHODCALLTYPE Global_vbPtrCmp(IGlobal *this, VARIANT vtPointer1, VARIANT vtPointer2, int *nCmpResult);
+
+#endif
