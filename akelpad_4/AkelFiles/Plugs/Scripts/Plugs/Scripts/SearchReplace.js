ccb8218427d0d8f9d2335e40312b6995e6695dff
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2015-01-06 16:21:32 +0000

    Fixed: syntax typo.

--- akelpad_4/AkelFiles/Plugs/Scripts/Plugs/Scripts/SearchReplace.js
+++ akelpad_4/AkelFiles/Plugs/Scripts/Plugs/Scripts/SearchReplace.js
@@ -1903,7 +1903,7 @@ function RestoreLineScroll(hWnd, nBeforeLine)
     if (lpScrollPos=AkelPad.MemAlloc(_X64?16:8 /*sizeof(POINT64)*/))
     {
       AkelPad.MemCopy(_PtrAdd(lpScrollPos, 0) /*offsetof(POINT64, x)*/, -1, 2 /*DT_QWORD*/);
-      AkelPad.MemCopy(_PtrAdd(lpScrollPos + _X64?8:4) /*offsetof(POINT64, y)*/, nPosY, 2 /*DT_QWORD*/);
+      AkelPad.MemCopy(_PtrAdd(lpScrollPos, _X64?8:4) /*offsetof(POINT64, y)*/, nPosY, 2 /*DT_QWORD*/);
       AkelPad.SendMessage(hWnd, 3180 /*AEM_SETSCROLLPOS*/, 0, lpScrollPos);
       AkelPad.MemFree(lpScrollPos);
     }
