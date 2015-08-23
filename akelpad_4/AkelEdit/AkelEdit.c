c3a223f25e786dfd3dc43dd6012d8041f6422550
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2015-08-23 17:57:51 +0000

    Updated.

--- akelpad_4/AkelEdit/AkelEdit.c
+++ akelpad_4/AkelEdit/AkelEdit.c
@@ -14588,14 +14588,14 @@ void AE_PaintCheckHighlightOpenItem(AKELEDIT *ae, AETEXTOUT *to, AEHLPAINT *hlp,
             AECHARINDEX ciDrawLine=to->ciDrawLine;
             DWORD dwSelFlag=(hlp->dwPaintType & AEHPT_SELECTION);
             DWORD dwColSelFlag=(to->dwPrintFlags & AEPRN_COLOREDSELECTION);
-          
+
             hlp->dwPaintType|=AEHPT_SELECTION;
             to->dwPrintFlags&=~AEPRN_COLOREDSELECTION;
-          
+
             for (to->ciDrawLine=hlp->qm.crQuoteStart.ciMax; AEC_IndexCompare(&to->ciDrawLine, &ciDrawLine) <= 0; AEC_NextCharInLine(&to->ciDrawLine))
             {
               AE_PaintCheckHighlightCloseItem(ae, to, hlp);
-          
+
               if (!(nFoundChild=AE_HighlightFindQuote(ae, &to->ciDrawLine, AEHF_FINDCHILD, &hlp->qm, &hlp->fm)))
                 nFoundChild=AE_HighlightFindQuoteRE(ae, &to->ciDrawLine, AEHF_FINDCHILD, &hlp->qm, &hlp->fm);
             }
