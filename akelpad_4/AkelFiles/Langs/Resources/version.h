94272ef7faccd6076cf9d2873260514794ddb964
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2018-04-01 01:03:11 +0000

    Version updating.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@4358 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelFiles/Langs/Resources/version.h
+++ akelpad_4/AkelFiles/Langs/Resources/version.h
@@ -7,7 +7,7 @@
     #else
       #define RC_VERSIONDESCRIPTION "AkelPad (x86) text editor"
     #endif
-    #define RC_VERSIONCOPYRIGHT "Copyright © AkelSoft 2003-2016"
+    #define RC_VERSIONCOPYRIGHT "Copyright © AkelSoft 2003-2018"
     #define RC_VERSIONNAME "AkelPad.exe"
     #undef _MAC
     #include "Version.rc"
