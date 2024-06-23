f93c02fe5a54f134e033be490b72ce44ca2892f7
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2024-06-23 11:29:32 +0000

    Fixed: missing Catalan and Greek languages in x64.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@4446 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelFiles/Langs/Build-All.cmd
+++ akelpad_4/AkelFiles/Langs/Build-All.cmd
@@ -1,15 +1,24 @@
+::Build.cmd /S /X32 /D
+:: /S   silent mode
+:: /X32 32-bit or /X64 64-bit
+:: /D   debug version
+
 @ECHO OFF
 set VCDIR=c:\Program Files\Microsoft Visual C++ Toolkit 2003
 set MSSDK=c:\Program Files\Microsoft Platform SDK
 set BIT=32
+set DEBUG=0
 
 ::### Set paths ###::
+if "%2" == "/X32" set BIT=32
+if "%2" == "/X64" set BIT=64
 if "%BIT%" == "32" (
   set "PATH=%MSSDK%\bin;%VCDIR%\bin;%VCDIR%\..\Common7\IDE;%VCDIR%\..\Common\MSDev98\Bin;%PATH%"
   set "INCLUDE=%MSSDK%\include;%MSSDK%\include\crt;%VCDIR%\include;%INCLUDE%"
   set "LIB=%MSSDK%\lib;%VCDIR%\lib;%LIB%"
   set MACHINE=I386
 ) else (
+  set VCDIR=%MSSDK%
   set "PATH=%MSSDK%\bin\win64\x86\amd64;%MSSDK%\bin;%VCDIR%\bin\x86_amd64;%VCDIR%\..\Common7\IDE;%PATH%"
   set "INCLUDE=%MSSDK%\include;%MSSDK%\include\crt;%VCDIR%\include;%INCLUDE%"
   set "LIB=%MSSDK%\lib\amd64;%MSSDK%\lib\x64;%VCDIR%\lib\amd64;%LIB%"
@@ -17,6 +26,18 @@ if "%BIT%" == "32" (
 )
 
 ::### Compile ###::
+if "%3" == "/D" set DEBUG=1
+if "%DEBUG%" == "1" (
+  set CLFLAGS=%CLFLAGS% /Od /Zi /DDEBUG
+  set LINKFLAGS=%LINKFLAGS% /DEBUG libcmt.lib
+  set RCFLAGS=%RCFLAGS% /D_DEBUG
+) else (
+  set CLFLAGS=%CLFLAGS% /O1
+)
+if "%VCDIR%" == "%VCDIR:2003=%" (
+  if "%VCDIR%" == "%VCDIR:VC98=%" set CLFLAGS=%CLFLAGS% /GS-
+)
+
 call :COMPILE "Belorussian" 0x423
 if not %ERRORLEVEL% == 0 goto END
 
@@ -93,11 +114,14 @@ goto :EOF
 :COMPILE
 set LANGNAME=%~1
 set LANGID=%2
-rc /R /DDLL_VERSION /DRC_VERSIONLANGID=%LANGID% /DRC_VERSIONBIT=%BIT% /I "..\..\AkelEdit\Resources" /Fo"%LANGNAME%.res" "Resources\%LANGNAME%.rc"
+rc %RCFLAGS% /R /DDLL_VERSION /DRC_VERSIONLANGID=%LANGID% /DRC_VERSIONBIT=%BIT% /I "..\..\AkelEdit\Resources" /Fo"%LANGNAME%.res" "Resources\%LANGNAME%.rc"
 if not %ERRORLEVEL% == 0 echo %LANGNAME%.rc FAILED!
 if not %ERRORLEVEL% == 0 goto :EOF
-cl /O1 Module.c /LD /link "%LANGNAME%.res" /OPT:NOWIN98 /MACHINE:%MACHINE% /NODEFAULTLIB /ENTRY:DllMain /OUT:"%LANGNAME%.dll"
+cl %CLFLAGS% Module.c /LD /link "%LANGNAME%.res" %LINKFLAGS% /OPT:NOWIN98 /MACHINE:%MACHINE% /NODEFAULTLIB /ENTRY:DllMain /OUT:"%LANGNAME%.dll"
 
 ::### Clean up ###::
 if exist Module.obj del Module.obj
 if exist "%LANGNAME%.res" del "%LANGNAME%.res"
+if exist "%LANGNAME%.ilk" del "%LANGNAME%.ilk"
+if exist "%LANGNAME%.pdb" del "%LANGNAME%.pdb"
+if exist vc?0.pdb del vc?0.pdb
