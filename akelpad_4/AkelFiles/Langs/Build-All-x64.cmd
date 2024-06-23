34ecbe17419f64874c2099765b6b1619174358a6
instructor_ <instructor_@24cdac72-59cb-4434-bdd7-86cdcaa6fc6e>
2024-06-23 11:31:37 +0000

    Fixed: missing Catalan and Greek languages in x64.
    git-svn-id: https://svn.code.sf.net/p/akelpad/codesvn/trunk@4447 24cdac72-59cb-4434-bdd7-86cdcaa6fc6e

--- akelpad_4/AkelFiles/Langs/Build-All-x64.cmd
+++ akelpad_4/AkelFiles/Langs/Build-All-x64.cmd
@@ -8,8 +8,8 @@ set FLAG_S=0
 set FLAG_X=/X64
 set FLAG_D=0
 if "%1" == "/S" set FLAG_S=/S
-if "%2" == "/X32" set FLAG_X=32
-if "%2" == "/X64" set FLAG_X=64
+if "%2" == "/X32" set FLAG_X=/X32
+if "%2" == "/X64" set FLAG_X=/X64
 if "%3" == "/D" set FLAG_D=/D
 
 call Build-All.cmd %FLAG_S% %FLAG_X% %FLAG_D%
